e3bc94222cf00512bb6e7f9cb32c1401a6a1b5de
Moses Bug <mbug@brandeis.edu>
2016-06-09 14:16:56 -0400

    added a non-functioning delete button

---
+++ pages/comments/client/comments.js
@@ -0,0 +1,36 @@
+Template.comments.helpers({
+	comments: function(){return Comments.find({},{sort:{createdAt:-1}, limit:5})}
+})
+
+Template.comments.events({
+	"click .js-submit-comment": function(event){
+		event.preventDefault();
+		console.dir(event);
+		const comment_text = $(".js-user-comment").val();
+		const comment_rating = $(".js-user-rating").val();
+		if (comment_text == "") {
+			window.alert("Comment empty!");
+			return;
+		}
+		const comment_obj =
+		{text: comment_text,
+		rating: comment_rating,
+		createdAt: new Date(),
+		createdBy: Meteor.userId(),
+		userEmail: Meteor.user().emails[0].address};
+		console.dir(comment_obj);
+
+		Comments.insert(comment_obj);
+		$(".js-user-comment").val("");
+		//Router.go('/');
+	}
+
+})
+
+Template.commentRow.events({
+	"click .js-delete-comment": function(event){
+		console.log("clicked on the x");
+		console.dir(this);
+		Comments.remove(this.comment_id);
+	}
+})
